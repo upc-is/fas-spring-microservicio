437bc6aba3eb53822a893a90889ae3ce34bf7a7d
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-09-12 12:58:57 -0500

    Started Service

--- venta-service/src/main/java/pe/edu/upc/venta/repositories/ClienteRepository.java
+++ venta-service/src/main/java/pe/edu/upc/venta/repositories/ClienteRepository.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.venta.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.venta.entities.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
---
+++ venta-service/src/main/java/pe/edu/upc/venta/services/ClienteService.java
@@ -0,0 +1,6 @@
+package pe.edu.upc.venta.services;
+
+import pe.edu.upc.venta.entities.Cliente;
+
+public interface ClienteService extends CrudService<Cliente, Long> {
+}
